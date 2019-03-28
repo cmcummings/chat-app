811b21466a1cd58d71764a42043d4a5fea7c877c
connor <cmcummings2003@gmail.com>
2019-03-28 08:42:12 -0500

    Added user login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+from exceptions import InvalidLoginError
 
 # Initialize flask
 from flask import Flask, render_template, url_for, session, redirect, request, escape
@@ -9,6 +10,10 @@ app.secret_key = b'JPtUKpetQiyfzGpBS5SM' # yeah, i don't care. hack me
 # Establish connection to the database
 from database import Connection
 db = Connection(app, 'localhost', 27017)
+
+# Initialize chatlogger
+from chatlog import Logger
+logger = Logger('chat.log')
 
 
 
@@ -25,14 +30,25 @@ def index():
 # Login page
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+
     if 'username' in session:
         return redirect(url_for('index'))
 
     if request.method == 'POST':
-        if request.form['username'] in {None, ""}:
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in {None, ""}:
             return redirect(url_for('login', error="nouser"))
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
+
+        try:
+            if db.validate_login(username, password):
+                session['username'] = username
+                return redirect(url_for('index'))
+            else:
+                return redirect(url_for('login', error='invalidlogin'))
+        except InvalidLoginError:
+            return redirect(url_for('login', error='invalidlogin'))
 
     if request.method == 'GET':
         error = request.args.get('error', '')
@@ -51,28 +67,33 @@ def logout():
 def room(name=None):
     if 'username' not in session:
         return redirect(url_for('login'))
+    if name is None:
+        return redirect(url_for('index'))
 
     messages = db.get_messages(name)
     return render_template('room.html', username=session['username'], room=name, messages=messages)
 
-@app.route('/send-message/', methods=['GET'])
+# Message functions
+@app.route('/send-message/', methods=['GET']) # Inserts the message into the database, then returns all the messages in a room
 def send_message():
     room = request.args.get('room', '')
     author = session['username']
     content = request.args.get('msg', '')
     
+    logger.add_entry(room, author, content)
     db.add_message(room, author, content)
-    return render_template('messages.html', messages=db.get_messages(room))
+    messages = db.get_messages(room)
+    return render_template('messages.html', messages=messages)
 
-@app.route('/get-messages/', methods=['GET'])
+@app.route('/get-messages/', methods=['GET']) # Returns all the messages in a room
 def get_messages():
     room = request.args.get('room', '')
    
     messages = db.get_messages(room)
-    return render_template('messages.html', messages=db.get_messages(room))
+    return render_template('messages.html', messages=messages)
 
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0') # Run on local network
